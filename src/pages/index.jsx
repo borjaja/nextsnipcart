a7742541cde8f24a6478e3e285d8eecc751060f7
Borja Alvarez <balvarez@orienteed.com>
2021-01-12 13:53:16 +0100

    all

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,50 +1,10 @@
-import Header from "../components/Header";
-import Plp from "./Plp";
-import Footer from "../components/Footer";
-import Head from "next/head";
-import styles from "./index.module.css";
-
-/*
-interface IIndexProps {
-  products: IProduct[]
-}*/
+import Layout from "../components/Layout/Layout";
 
 const Index = (props) => {
     return (
-        <div className={styles.app}>
-            <Head>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-                <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key={process.env.SNIPCART_APIKEY} id="snipcart"></script>
-                <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
-            </Head>
-            <Header />
-            <main className="main">
-                <Plp products={props.products} />
-            </main>
-            <Footer />
-        </div>
+        <Layout>
+            <div>Contenido principal</div>
+        </Layout>
     );
 };
-/*
-Index.getInitialProps = async ({req}) => {
-    return {
-        products: [
-            {
-                id: "nextjs_halfmoon",
-                name: "Halfmoon Betta",
-                price: 25.0,
-                image: "../static/halfmoon.jpg",
-                description: "The Halfmoon betta is arguably one of the prettiest betta species. It is recognized by its large tail that can flare up to 180 degrees.",
-            },
-            {
-                id: "nextjs_halfmoon",
-                name: "Halfmoon Betta",
-                price: 25.0,
-                image: "../static/halfmoon.jpg",
-                description: "The Halfmoon betta is arguably one of the prettiest betta species. It is recognized by its large tail that can flare up to 180 degrees.",
-            },
-            //{...}
-        ],
-    };
-};*/
 export default Index;
---
+++ src/pages/products/index.jsx
@@ -0,0 +1,24 @@
+import Card from "../../components/Card/Card";
+import Layout from "../../components/Layout/Layout";
+import styles from "./products.module.scss";
+
+const Plp = ({products}) => {
+    console.log(products);
+    return (
+        <Layout>
+            <div className={styles.product_list}>
+                {products.map((product, index) => (
+                    <Card product={product} key={index} />
+                ))}
+            </div>
+        </Layout>
+    );
+};
+
+export async function getStaticProps(context) {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const products = await res.json();
+    return {props: {products}};
+}
+
+export default Plp;
