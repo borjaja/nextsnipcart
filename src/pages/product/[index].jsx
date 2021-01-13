ec30c5e1f22713946e0e3de44dcc755e02f017a8
Borja Alvarez <balvarez@orienteed.com>
2021-01-13 10:03:18 +0100

    test 2

--- src/pages/product/[index].jsx
+++ src/pages/product/[index].jsx
@@ -49,9 +49,27 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({params}) {
-    const res = await fetch(`https://fakestoreapi.com/products/${params.index}`);
-    const product = await res.json();
-    return {props: {product}};
+    //const res = await fetch(`https://fakestoreapi.com/products/${params.index}`);
+    //const product = await res.json();
+    const product1 = {
+        id: 1,
+        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        price: 109.95,
+        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        category: "men clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    };
+    const product2 = {
+        id: 2,
+        title: "Mens Casual Premium Slim Fit T-Shirts ",
+        price: 22.3,
+        description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        category: "men clothing",
+        image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    };
+
+    return params.index === 1 ? product1 : product2;
+    //return {props: {product}};
 }
 
 export default withRouter(Pdp);
